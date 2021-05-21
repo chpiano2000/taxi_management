32885a95eaff3b5efd58c51aad888d9059fd9369
VCD <dat.usth2000@gmail.com>
2021-05-21 21:59:00 +0700

    add the driver stuff

--- main.py
+++ main.py
@@ -8,15 +8,130 @@ import pymongo
 
 from PyQt5.uic import loadUiType
 
-ui,_ = loadUiType('GUI/taxi.ui')
-login,_ = loadUiType('GUI/login.ui')
-signup,_ = loadUiType('GUI/signup.ui')
+begin,_ = loadUiType('GUI/begin.ui')
+ui_user,_ = loadUiType('GUI/main_user.ui')
+ui_driver,_ = loadUiType('GUI/main_driver.ui')
+login_user,_ = loadUiType('GUI/login_user.ui')
+login_driver,_ = loadUiType('GUI/login_driver.ui')
+signup_user,_ = loadUiType('GUI/signup_user.ui')
+signup_driver,_ = loadUiType('GUI/signup_driver.ui')
 
 url = 'mongodb+srv://todoAppUser:Leanbichphuong0702@cluster0.oeozu.mongodb.net/TaxniManegement?retryWrites=true&w=majority'
 mongo = pymongo.MongoClient(url)
 
 
-class Signup(QWidget, signup):
+class Begin(QWidget, begin):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.Move_To_User)
+        self.pushButton_2.clicked.connect(self.Move_To_Driver)
+        self.pushButton_3.clicked.connect(self.Move_To_Admin)
+    
+    def Move_To_User(self):
+        self.window2 = Login_User()
+        self.close()
+        self.window2.show()
+    
+    def Move_To_Driver(self):
+        self.window2 = Login_Driver()
+        self.close()
+        self.window2.show()
+
+    def Move_To_Admin(self):
+        pass
+
+######################## Driver #####################################
+class Signup_Driver(QWidget, signup_driver):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.Handel_Signup)
+        self.pushButton_2.clicked.connect(self.Switch_To_Login)
+    
+    def Handel_Signup(self):
+        self.db = mongo.taxi_management
+
+        name = self.lineEdit.text()
+        gmail = self.lineEdit_5.text()
+        sex = self.comboBox.currentText()
+        password = self.lineEdit_3.text()
+        confirm_password = self.lineEdit_4.text()
+        car = self.lineEdit_5.text()
+
+        data = {
+            "name": name,
+            "sex": sex,
+            "car": car,
+            "gmail": gmail,
+            "password": password,
+            "histories": [],
+            "star": []
+        }
+        
+        checker = list(self.db.drivers.find({"gmail": gmail}))
+
+        if gmail == '' or password == '' or name=='' or confirm_password == '':
+            self.label_6.setText('Please fill in the form')
+        elif password != confirm_password:
+            self.label_6.setText('Passwords not match')
+        elif checker == []:
+            self.db.users.insert_one(data)
+            self.window2 = MainApp_Driver(gmail=gmail)
+            self.close()
+            self.window2.show()
+        else: 
+            self.label_6.setText("Gmail existed")
+    
+    def Switch_To_Login(self):
+        self.window2 = Login_Driver()
+        self.close()
+        self.window2.show()
+
+class Login_Driver(QWidget, login_driver):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.Handel_Login)
+        self.pushButton_2.clicked.connect(self.Switch_To_Signup)
+    
+    def Handel_Login(self):
+        self.db = mongo.taxi_management
+        gmail = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+
+        data = list(self.db.drivers.find({"gmail": gmail}))
+
+        if gmail == '' or password == '' :
+            self.label.setText('Please fill in the form')
+        elif data :
+            if password != data[0]['password']:
+                self.label.setText('Password or Email is not match')
+            else: 
+                self.window2 = MainApp_Driver(gmail=gmail)
+                self.close()
+                self.window2.show()
+        else:
+            self.label.setText('Password or Gmail is not match')
+        
+    def Switch_To_Signup(self):
+        self.window2 = Signup_Driver()
+        self.close()
+        self.window2.show()
+    
+class MainApp_Driver(QMainWindow, ui_driver):
+    def __init__(self, gmail):
+        self.gmail = gmail
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+    
+######################## Admin #####################################
+
+
+######################## USER #####################################
+
+class Signup_User(QWidget, signup_user):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
@@ -48,20 +163,19 @@ class Signup(QWidget, signup):
             self.label_6.setText('Passwords not match')
         elif checker == []:
             self.db.users.insert_one(data)
-            self.window2 = MainApp()
+            self.window2 = MainApp_User(gmail=gmail)
             self.close()
-            self.window2.setEmail(gmail)
             self.window2.show()
         else: 
             self.label_6.setText("Gmail existed")
     
     def Switch_To_Login(self):
-        self.window2 = Login()
+        self.window2 = Login_User()
         self.close()
         self.window2.show()
 
 
-class Login(QWidget, login):
+class Login_User(QWidget, login_user):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setupUi(self)
@@ -81,22 +195,21 @@ class Login(QWidget, login):
             if password != data[0]['password']:
                 self.label.setText('Password or Email is not match')
             else: 
-                self.window2 = MainApp()
+                self.window2 = MainApp_User(gmail=gmail)
                 self.close()
-                self.window2.setEmail(gmail)
                 self.window2.show()
         else:
             self.label.setText('Password or Gmail is not match')
         
     def Switch_To_Signup(self):
-        self.window2 = Signup()
+        self.window2 = Signup_User()
         self.close()
         self.window2.show()
         
 
-class MainApp(QMainWindow, ui):
-    mail = ""
-    def __init__(self):
+class MainApp_User(QMainWindow, ui_user):
+    def __init__(self, gmail):
+        self.gmail = gmail
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.Handel_UI_Changes()
@@ -106,9 +219,6 @@ class MainApp(QMainWindow, ui):
         self.dateTimeEdit.setDateTime(QDateTime.currentDateTime())
 
         self.Show_Booking_Info()
-
-    def setEmail(self, mail):
-        gmail = mail
 
     def Handel_UI_Changes(self):
         self.Hiding_Themes()
@@ -165,7 +275,7 @@ class MainApp(QMainWindow, ui):
             self.statusBar().showMessage('Please fill in the form')
         elif password != confirm_password:
             self.statusBar().showMessage('Password is not match')
-        elif list(self.db.users.find({'password': password})):
+        elif list(self.db.users.find({"gmail": self.gmail})):
             self.db.users.update_one({'password': password}, {"$set": {"name": name, "gmail": gmail}})
             self.statusBar().showMessage('Information Changed')
         
@@ -205,7 +315,7 @@ class MainApp(QMainWindow, ui):
         elif destination == '':
             self.statusBar().showMessage('Please fill in the destination')
         else:
-            self.db.users.update_one({"name": 'aiden'}, {"$push": {'histories': data}})
+            self.db.users.update_one({"gmail": self.gmail}, {"$push": {'histories': data}})
         
         location = self.lineEdit.setText('')
         destination = self.lineEdit_2.setText('')
@@ -288,7 +398,7 @@ class MainApp(QMainWindow, ui):
         Logout button
     """
     def Log_Out(self):
-        self.window2 = Login()
+        self.window2 = Begin()
         self.close()
         self.window2.show()
 
@@ -296,7 +406,7 @@ class MainApp(QMainWindow, ui):
 
 def main():
     app = QApplication(sys.argv)
-    window = Login()
+    window = Begin()
     window.show()
     app.exec_()
 
